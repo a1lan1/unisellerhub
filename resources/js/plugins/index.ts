37239c59585f444bd046140ba8d0c5fb0ab80bd5
Aleksandr Smirnov <genesboard@gmail.com>
2026-04-04 18:04:01 +0300

    add pinia with plugin

--- resources/js/plugins/index.ts
+++ resources/js/plugins/index.ts
@@ -1,10 +1,12 @@
 import type { App } from 'vue'
 import axios from './axios'
 import './echo'
+import pinia from './pinia'
 import vuetify from './vuetify'
 
 export function registerPlugins(app: App) {
   app
     .use(axios)
+    .use(pinia)
     .use(vuetify)
 }
