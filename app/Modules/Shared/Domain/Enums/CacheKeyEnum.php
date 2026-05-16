85456bec24aab302f84299f21f5f76e23c99ceec
Aleksandr Smirnov <genesboard@gmail.com>
2026-05-16 13:22:17 +0300

    update CacheKeyEnum with geo keys

--- app/Modules/Shared/Domain/Enums/CacheKeyEnum.php
+++ app/Modules/Shared/Domain/Enums/CacheKeyEnum.php
@@ -8,4 +8,11 @@ enum CacheKeyEnum: string
 {
     case USER_PERMISSIONS = 'user_permissions_%d';
     case USER_ROLES = 'user_roles_%d';
+    case REVIEWS_USER = 'reviews_user_%d_%s_page_%d';
+    case SELLERS = 'sellers';
+    case SELLERS_WITH_PRODUCTS = 'sellers_with_products_%d';
+    case LOCATIONS_FOR_USER = 'locations_for_user_%d';
+    case LOCATIONS_WITH_STATS = 'locations_with_stats_%d';
+    case RESPONSE_TEMPLATES_FOR_USER = 'response_templates_for_user_%d';
+    case GEO_METRICS_USER = 'geo_metrics_user_%d_loc_';
 }
