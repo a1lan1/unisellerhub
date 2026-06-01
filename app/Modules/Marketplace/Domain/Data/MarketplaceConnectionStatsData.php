922b860e9df5cea56d4ffd5a88cbc061298ddada
Aleksandr Smirnov <genesboard@gmail.com>
2026-06-01 19:47:28 +0300

    update MarketplaceConnectionStatsData with toArray method and request capture

--- app/Modules/Marketplace/Domain/Data/MarketplaceConnectionStatsData.php
+++ app/Modules/Marketplace/Domain/Data/MarketplaceConnectionStatsData.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace App\Modules\Marketplace\Domain\Data;
 
 use App\Modules\Activity\Interfaces\Http\Resources\ActivityResource;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Override;
+use Spatie\LaravelData\Data;
 
-class MarketplaceConnectionStatsData
+class MarketplaceConnectionStatsData extends Data
 {
     /**
      * @param  AnonymousResourceCollection<int, ActivityResource>  $recentActivity
@@ -17,4 +20,14 @@ class MarketplaceConnectionStatsData
         public int $totalOrders,
         public AnonymousResourceCollection $recentActivity,
     ) {}
+
+    #[Override]
+    public function toArray(): array
+    {
+        return [
+            'totalProducts' => $this->totalProducts,
+            'totalOrders' => $this->totalOrders,
+            'recentActivity' => $this->recentActivity->toArray(Request::capture()),
+        ];
+    }
 }
